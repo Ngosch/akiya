4187e1a1606340ecb57340b5f807c7bdb517c575
Ngosch <carl1312@gmail.com>
2023-11-17 00:18:15 +0900

    before_location_change

--- mobara_scraper.py
+++ mobara_scraper.py
@@ -1,11 +1,32 @@
+from flask import Flask, jsonify, send_from_directory
 import requests
 from bs4 import BeautifulSoup
+from flask_cors import CORS  # CORSをインポート
 
-url = 'https://www.city.mobara.chiba.jp/0000007702.html'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
 
-# 指定されたセレクタを使用してPDFのリンクを取得
-pdf_link = soup.select_one('#mol_contents > div.mol_attachfileblock.block_index_4 > ul > li:nth-child(2) > a')['href']
+app = Flask(__name__)
+CORS(app)  # CORSの設定を追加
 
-print(pdf_link)
+@app.route('/')
+def index():
+    return "Welcome to the Mobara Scraper API!"
+
+@app.route('/get_mobara_pdf')
+def get_mobara_pdf():
+    try:
+        url = 'https://www.city.mobara.chiba.jp/0000007702.html'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        relative_pdf_link = soup.select_one('#mol_contents > div.mol_attachfileblock.block_index_4 > ul > li:nth-child(2) > a')['href']
+        pdf_link = 'https://www.city.mobara.chiba.jp' + relative_pdf_link.replace('./', '/')
+        return jsonify({'pdfLink': pdf_link})
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+        return jsonify({'error': str(e)})
+
+@app.route('/js/<path:filename>')
+def custom_static_for_js(filename):
+    return send_from_directory('js', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/path/to/mobara_scraper.py')  // mobara_scraper.py の結果を取得するサーバーサイドのエンドポイント
+    fetch('http://127.0.0.1:5000/get_mobara_pdf')  // Flask APIのエンドポイント
         .then(response => response.json())
         .then(data => {
             const pdfLink = data.pdfLink;
             const linkElement = document.createElement('a');
             linkElement.href = pdfLink;
-            linkElement.textContent = '最新の物件情報PDF';
+            linkElement.textContent = '最新の物件情報PDFを見る';
+            linkElement.target = '_blank'; // 新しいタブでPDFを開く
             document.getElementById('mobara-property').appendChild(linkElement);
         })
         .catch(error => console.error('Error:', error));
