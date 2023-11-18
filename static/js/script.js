378862674c58643f3a2a774527851708aa26c4fc
Ngosch <carl1312@gmail.com>
2023-11-19 00:53:54 +0900

    chiba add

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     fetch('http://127.0.0.1:5000/get_mobara_pdf')
         .then(response => response.json())
         .then(data => {
@@ -32,4 +32,32 @@ document.addEventListener('DOMContentLoaded', function() {
             cell2.appendChild(link2);
         })
         .catch(error => console.error('Error:', error));
+
+    // 千葉市の公式サイトへのリンクを追加
+    const chibaCitySection = document.getElementById('chiba-city-section');
+    const chibaLink = document.createElement('a');
+    chibaLink.href = 'https://www.cjkk.or.jp/reuse/reuse_02.html';
+    chibaLink.textContent = '千葉市空き家バンク公式サイト';
+    chibaLink.target = '_blank';
+    chibaCitySection.appendChild(chibaLink);
+
+    // 千葉県の市町村のリスト
+    const municipalities = [
+        "千葉市", "銚子市", "市川市", "船橋市", "館山市",
+        "木更津市", "松戸市", "野田市", "成田市",
+        "佐倉市", "東金市", "旭市", "習志野市", "柏市",
+        "勝浦市", "市原市", "流山市", "八千代市", "我孫子市",
+        "鴨川市", "鎌ケ谷市", "君津市", "富津市", "浦安市",
+        "四街道市", "袖ケ浦市", "八街市", "印西市", "白井市",
+        "富里市", "南房総市", "匝瑳市", "香取市", "山武市",
+        "いすみ市", "大網白里市", "印旛郡", "香取郡"
+    ];
+
+    // 千葉県の市町村の見出しを追加
+    const container = document.getElementById('municipalities-container');
+    municipalities.forEach(municipality => {
+        const heading = document.createElement('h3');
+        heading.textContent = `${municipality}の物件情報`;
+        container.appendChild(heading);
+    });
 });
